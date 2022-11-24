8996d6abbb26c6ae99cea4a94182ed2ca225dab0
Xavi <xcalduch@gmail.com>
2022-11-24 13:08:39 +0100

    Start Sprint 7

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div classname="App">
+  <div className="form-check">
+    <input
+      className="form-check-input"
+      type="checkbox"
+      defaultValue=""
+      id="flexCheckDefault"
+    />
+    <label className="form-check-label" htmlFor="flexCheckDefault">
+      Casilla de verificación por defecto
+    </label>
+  </div>
+  <div className="form-check">
+    <input
+      className="form-check-input"
+      type="checkbox"
+      defaultValue=""
+      id="flexCheckChecked"
+      defaultChecked=""
+    />
+    <label className="form-check-label" htmlFor="flexCheckChecked">
+      Casilla de verificación marcada
+    </label>
+  </div>
+  <div className="form-check">
+    <input
+      className="form-check-input"
+      type="checkbox"
+      defaultValue=""
+      id="flexCheckChecked"
+      defaultChecked=""
+    />
+    <label className="form-check-label" htmlFor="flexCheckChecked">
+      Casilla de verificación marcada
+    </label>
+  </div>
+</div>
   );
 }
 
