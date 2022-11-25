50ea1a9e5bf4eac45039d6c101cb925ae8108736
Xavi <xcalduch@gmail.com>
2022-11-25 17:00:43 +0100

    working Exercise 1

--- src/App.js
+++ src/App.js
@@ -1,47 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React, { useState } from 'react'
+import { services } from './assets/services';
+
+//const getFormattedPrice = price => `$${price.toFixed(2)}`;
+const getFormattedPrice = price => `$${price}`;
 
 function App() {
-  return (
-<div classname="App">
-  <div className="form-check">
-    <input
-      className="form-check-input"
-      type="checkbox"
-      defaultValue=""
-      id="flexCheckDefault"
-    />
-    <label className="form-check-label" htmlFor="flexCheckDefault">
-      Casilla de verificación por defecto
-    </label>
-  </div>
-  <div className="form-check">
-    <input
-      className="form-check-input"
-      type="checkbox"
-      defaultValue=""
-      id="flexCheckChecked"
-      defaultChecked=""
-    />
-    <label className="form-check-label" htmlFor="flexCheckChecked">
-      Casilla de verificación marcada
-    </label>
-  </div>
-  <div className="form-check">
-    <input
-      className="form-check-input"
-      type="checkbox"
-      defaultValue=""
-      id="flexCheckChecked"
-      defaultChecked=""
-    />
-    <label className="form-check-label" htmlFor="flexCheckChecked">
-      Casilla de verificación marcada
-    </label>
-  </div>
-</div>
-  );
-}
 
+
+  // Creo tantas variables como servicios tengo en el array y sus estados a false
+  const [checkedState, setCheckedState] = useState(
+    new Array(services.length).fill(false)
+);
+  const { total, setTotal} = useState(0);
+
+  const handleOnChange = (position) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
+
+    setCheckedState(updatedCheckedState);
+
+    const totalPrice = updatedCheckedState.reduce(
+      (sum, currentState, index) => {
+        if (currentState === true) {
+          return sum + services[index].price;
+        }
+        return sum;
+      },
+      0
+    );
+
+    setTotal(totalPrice);
+  };
+
+
+
+  return (
+    <div className="App">
+       <h3>Que dessitjes que fem?</h3>
+      <ul className="list-group mx-4">
+        {services.map(({ task, price }, index) => {
+          return (
+            <li key={index}>
+              <div className="task-list-item">
+                
+                  <input
+                    className='form-check-input'
+                    type="checkbox"
+                    id={`custom-checkbox-${index}`}
+                    name={task}
+                    value={task}
+                    checked={checkedState[index]}
+                    onChange={() => handleOnChange(index)}
+                  />
+                  <label htmlFor={`custom-checkbox-${index}`}>{task}</label>
+                
+                <div className="right-section">{getFormattedPrice(price)}</div>
+              </div>
+            </li>
+          );
+        })}
+        <li>
+          <div className="toppings-list-item">
+            <div className="left-section">Total:</div>
+            <div className="right-section">{getFormattedPrice(total)}</div>
+          </div>
+        </li>
+      </ul>
+   
+
+
+
+
+  </div>
+);
+}
 export default App;
---
+++ src/assets/services.js
@@ -0,0 +1,23 @@
+ export const services  = [
+    {
+    id:  1,
+    task: 'Una pàgina web 500€',
+    price: 500,
+    pages: 1,
+    lang: 1
+    },
+    {
+    id:  2,
+    task: 'Una consultoria SEO 300€',
+    price: 300,
+    pages: 0,
+    lang: 0
+    },
+    {
+    id:  3,
+    task: 'Una campanya de Gogle Ads 200€',
+    price: 200,
+    pages: 0,
+    lang: 0
+    }
+];
