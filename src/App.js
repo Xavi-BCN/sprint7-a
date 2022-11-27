3cd71cb4a3e35133edc42527d6758355eb802edd
Xavi <xcalduch@gmail.com>
2022-11-27 21:41:08 +0100

    Exercise 1 OK

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { services } from './assets/services';
+import './App.css'
 
-//const getFormattedPrice = price => `$${price.toFixed(2)}`;
-const getFormattedPrice = price => `$${price}`;
+const getFormattedPrice = (price) => `${price.toFixed(2)}€`;
 
-function App() {
+export default function App() {
 
-
+  
   // Creo tantas variables como servicios tengo en el array y sus estados a false
   const [checkedState, setCheckedState] = useState(
     new Array(services.length).fill(false)
-);
-  const { total, setTotal} = useState(0);
+  );
 
+  const [ total, setTotal ] = useState(0);
+
+  // Cuando cambie un checkbox actualizo su valor al contario del que tenia en un nuevo array
   const handleOnChange = (position) => {
     const updatedCheckedState = checkedState.map((item, index) =>
       index === position ? !item : item
     );
 
+    //paso el nuevo array como actualizacion de la variable y esto obliga a repintar pantalla 
     setCheckedState(updatedCheckedState);
 
+    //Calculo el total cogiendo el index de aquellos valores que sean true, y con ese index voy al array de servicios y cojo su precio.
     const totalPrice = updatedCheckedState.reduce(
       (sum, currentState, index) => {
         if (currentState === true) {
@@ -30,50 +34,38 @@ function App() {
       },
       0
     );
-
+    
+    //Paso el precio total calculado a la funcion setTotal para que repinte y muestre el total actualizado en pantalla.
     setTotal(totalPrice);
+    
   };
-
-
 
   return (
     <div className="App">
-       <h3>Que dessitjes que fem?</h3>
+      <h3>Que dessitjes que fem?</h3>
       <ul className="list-group mx-4">
+        {/* Cargamos los servicios con su control Checkbox  */}
         {services.map(({ task, price }, index) => {
           return (
             <li key={index}>
               <div className="task-list-item">
-                
-                  <input
-                    className='form-check-input'
-                    type="checkbox"
-                    id={`custom-checkbox-${index}`}
-                    name={task}
-                    value={task}
-                    checked={checkedState[index]}
-                    onChange={() => handleOnChange(index)}
-                  />
-                  <label htmlFor={`custom-checkbox-${index}`}>{task}</label>
-                
-                <div className="right-section">{getFormattedPrice(price)}</div>
+                <input
+                  className='form-check-input'
+                  type="checkbox"
+                  id={`custom-checkbox-${index}`}
+                  name={task}
+                  value={task}
+                  checked={checkedState[index]}
+                  onChange={() => handleOnChange(index)}
+                />
+                <label htmlFor={`custom-checkbox-${index}`}>{task}  {price}€</label>
               </div>
             </li>
           );
         })}
-        <li>
-          <div className="toppings-list-item">
-            <div className="left-section">Total:</div>
-            <div className="right-section">{getFormattedPrice(total)}</div>
-          </div>
-        </li>
       </ul>
-   
-
-
-
-
-  </div>
-);
+      <div className="total-section">Total: {getFormattedPrice(total)}</div>
+    </div>
+  );
 }
-export default App;
+
--- src/assets/services.js
+++ src/assets/services.js
@@ -1,21 +1,21 @@
  export const services  = [
     {
     id:  1,
-    task: 'Una pàgina web 500€',
+    task: 'Una pàgina web',
     price: 500,
     pages: 1,
     lang: 1
     },
     {
     id:  2,
-    task: 'Una consultoria SEO 300€',
+    task: 'Una consultoria SEO',
     price: 300,
     pages: 0,
     lang: 0
     },
     {
     id:  3,
-    task: 'Una campanya de Gogle Ads 200€',
+    task: 'Una campanya de Gogle Ads',
     price: 200,
     pages: 0,
     lang: 0
