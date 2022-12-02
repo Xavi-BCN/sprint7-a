c728088839de67932a28aca77a9c9ff99a24d8d1
Xavi <xcalduch@gmail.com>
2022-12-02 16:38:11 +0100

    Exercise 2 OK

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,24 +1,17 @@
 //import { useState } from "react";
-//import styled from "styled-components";
+import { StylPanel } from "./PanelStyle";
 import { services } from "../assets/services";
 
 
 
 function Panel ({paginas, idiomas, view}){
   /*  const PanelStyle = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-    border: 2px solid black;
-    border-radius: 10px;
-    padding: 30px 5px;
-    width: 450px;
+   
   `;  */
     
   return (
     <>
-      {/* <PanelStyle> */}
+      <StylPanel>
       <p style={{display: 'inline'}}>Número de pàgines:</p>
       <input
         style={{width: '40%', margin: '10px'}}
@@ -39,7 +32,7 @@ function Panel ({paginas, idiomas, view}){
         aria-label=".form-control-sm example"
         onChange={ (event) => idiomas(event.target.value)}
       />
-      {/* </PanelStyle> */}
+      </StylPanel>
     </>
   );
 }
