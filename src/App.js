1723a54fdbf36c3d823dc501f6ec7c43f102c98e
Xavi <xcalduch@gmail.com>
2022-12-10 14:20:23 +0100

    Exercise 4 Refactor nothing more

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { useState,useEffect } from 'react'
 import Panel from './components/Panel';
 import './Styles/App.css'
 
-let suma = 0;
 let sumaweb = 0;
 let pricePagesAndLang = 0
 let existBudget;
+let suma = 0;
 
 
 export default function App() {
+
   
   const [web, setWeb] = useState(false);
   const [seo, setSeo] = useState(false);
@@ -27,46 +28,41 @@ export default function App() {
     setLang(numOfLang);
   }
   
-  const handleOnChange = (service) => {
-    
-    if(service.checked) {
-      
-      if(service.name === 'Web'){
-        
-        pricePagesAndLang = 30
-        suma += parseInt(service.value) 
-        setWeb(true)
-      }        
-      
-      if(service.name === 'Seo'){
-        suma += parseInt(service.value);
-        setSeo(true)
-      }
-      if(service.name === 'Ads'){
-        suma += parseInt(service.value);
-        setAds(true)
-      }
+  const handleOnChangeWeb = (checked, value) => {
+    if(checked){
+      pricePagesAndLang = 30    
+      suma += parseInt(value) 
+      setWeb(true)
     }else{
-      if(service.name === 'Web'){
-        suma -= parseInt(service.value)
-        setPages(1)
-        setLang(1)
-        setWeb(false)
-        
-        pricePagesAndLang = 0
-      }else{
-        if(service.name === 'Seo'){
-          suma -= parseInt(service.value)
-          setSeo(false)
-        }else{
-          suma -= parseInt(service.value)
-          setAds(false)
-        }        
-      }        
-    }  
-  };
+      suma -= parseInt(value)
+      setPages(1)
+      setLang(1)
+      setWeb(false)
+      pricePagesAndLang = 0
+    }
+  }
 
-  
+  const handleOnChangeSeo = (checked, value) => {
+    if(checked){
+      suma += parseInt(value) 
+      setSeo(true)
+    }else{
+      suma -= parseInt(value)
+      setSeo(false)
+    }
+  }
+
+  const handleOnChangeAds = (checked, value) => {
+    if(checked){
+      suma += parseInt(value) 
+      setAds(true)
+    }else{
+      suma -= parseInt(value)
+      setAds(false)
+    }
+  }
+
+
   useEffect(() => {
     existBudget = JSON.parse(localStorage.getItem('Presupuesto'));
     if (existBudget) {
@@ -87,7 +83,6 @@ export default function App() {
   },[listBudgets])
   
    const loadData = (existBudget) => {
-    
     setWeb(existBudget[0].web)
     setPages(existBudget[0].pages);
     setLang(existBudget[0].lang);
@@ -97,7 +92,6 @@ export default function App() {
     }
   
   const saveData = ()=>{
-
     setListBudgets([...listBudgets, {web, pages, lang, seo, ads, total}]);  
  }
  
@@ -111,7 +105,8 @@ export default function App() {
             value={500}
             name='Web'
             checked={web}
-            onChange={ (e) => handleOnChange(e.target)}
+            //onChange={ (e) => handleOnChange(e.target)}
+            onChange={ (e) => handleOnChangeWeb(e.target.checked, e.target.value)}
             /><h5 style={{ display: "inline" }}> Una pàgina Web (500€)</h5><br/><br/>
             {web && (<>
             <Panel paginas={handlePages} idiomas={handleLang}/><br/></>)}
@@ -121,7 +116,8 @@ export default function App() {
             value={300}
             name='Seo'
             checked={seo}
-            onChange={ (e) => handleOnChange(e.target)}
+            //onChange={ (e) => handleOnChange(e.target)}
+            onChange={ (e) => handleOnChangeSeo(e.target.checked, e.target.value)}
           /><h5 style={{ display: "inline" }}> Una consultora SEO (300€)</h5><br/><br/>
           <input
             className='form-check-input'
@@ -129,7 +125,8 @@ export default function App() {
             value={200}
             name='Ads'
             checked={ads}
-            onChange={ (e) => handleOnChange(e.target)}
+            //onChange={ (e) => handleOnChange(e.target)}
+            onChange={ (e) => handleOnChangeAds(e.target.checked, e.target.value)}
           /><h5 style={{ display: "inline" }}> Una campanya de Google Ads (200€)</h5><br/><br/>
       <div className="total-section" defaultValue={total}>Total:{total}€</div><br/><br/>
       <button
