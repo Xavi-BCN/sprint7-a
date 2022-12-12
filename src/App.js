dcc1b355f6ea098c151c06692a70e352c0754e7b
Xavi <xcalduch@gmail.com>
2022-12-12 17:22:08 +0100

    Exercise 5 Navbar mounted but not run

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import Routes from './application/routes';
+import { Navbar } from './components/Navbar';
 import './Styles/App.css'
 
-const App = () => <Routes/>
+const App = () => {
+
+return (
+    <>
+    <Navbar/>
+    <Routes/>
+    </>
+
+)
+}
+
   
 
 export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+export const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-link active" aria-current="page" href="#">
+                Inicio
+              </a>
+              <a className="nav-link" href="#">
+                Características
+              </a>
+              <a className="nav-link" href="#">
+                Precios
+              </a>
+              <a className="nav-link disabled">Deshabilitado</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
